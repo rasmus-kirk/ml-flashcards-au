8bd84c506c24eddf6777ff1ed80d45f305d6fd61
rasmus-kirk <mail@rasmuskirk.com>
2023-05-05 10:10:43 +0200

    Added experimential flake support

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A report built with Pandoc, XeLaTex and a custom font";
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.report = (
+      let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+        #fonts = pkgs.makeFontsConf { fontDirectories = [ pkgs.dejavu_fonts ]; };
+      in
+        pkgs.stdenv.mkDerivation {
+          name = "markdown-flash-cards";
+          src = ./.;
+          buildInputs = with pkgs; [
+            markdown-anki-decks
+          ];
+          phases = ["unpackPhase" "buildPhase"];
+          buildPhase = ''
+            mkdir -p $out
+            unset SOURCE_DATE_EPOCH
+            SOURCE_DATE_EPOCH=315532800 mdankideck input $out
+          '';
+        }
+    );
+
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.report;
+    };
+}
